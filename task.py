05595a9fe3803ce7b36951a34448851c6d7c8662
Rushil Shah <rps1073@gmail.com>
2023-04-16 20:39:51 -0400

    can pull sample data for yesterday at an hourly level

--- task.py
+++ task.py
@@ -1,26 +1,33 @@
 import psycopg2
 import pandas as pd
-import os
+import json
+import requests
 from decouple import config
+from datetime import datetime, timedelta
 from sqlalchemy import create_engine
+from model.hourly_forecast import hourly_forecast
 
 print('Starting task')
-db_config = {
+app_config = {
     "user": config('username'),
     "password": config('password'),
     "host": config('host'),
     "database": config('database'),
+    "api_key": config('api_key'),
 }
 
 try:
-    conn_string = f'postgresql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["database"]}'
-    db = create_engine(conn_string)
-    conn = db.connect()
+    yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+    print(f"Weather Date: {yesterday}")
+    url = f"https://api.weatherapi.com/v1/history.json?key={app_config['api_key']}&q=Somerville&dt={yesterday}"
 
-    data = pd.read_sql('SELECT * FROM covid_metrics_raw.daily_cases_average LIMIT 1;', conn)
-    print(data)
-    
-    conn.close()
+    response = requests.request("GET", url, headers={}, data={})
+    data = response.json()
+    day_forecast = data['forecast']['forecastday'][0]['hour']
+    forecast_df = pd.DataFrame(day_forecast)
+    forecast_df_formatted = hourly_forecast(forecast_df)
+    print(forecast_df_formatted)
+
 except Exception as e:
     print(e)
 
