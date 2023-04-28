432762ffadf19af94248d80c1def72d933ab3818
Rushil Shah <rps1073@gmail.com>
2023-04-28 17:41:58 -0400

    add flake8 and black + ran black formatter

--- model/parse_daily_forecast_test.py
+++
@@ -1,8 +0,0 @@
-from parse_daily_forecast import parse_daily_forecast
-import pandas as pd
-
-def test_parse_daily_forecast():
-    df = pd.DataFrame()
-    location_series = pd.Series()
-    print(df)
-    assert parse_daily_forecast(df, location_series) == 4
