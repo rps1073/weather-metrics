a432b6682c5b491943863cedb3e864214d2b3149
Rushil Shah <rps1073@gmail.com>
2023-04-28 16:17:58 -0400

    switch to using os env variables + finish snowflake connection

--- model/parse_daily_forecast.py
+++ model/parse_daily_forecast.py
@@ -3,28 +3,25 @@ import pandas as pd
 def parse_daily_forecast(df, location_series):
     df_formatted = df[['date']].rename(
         columns={
-            'date': 'forecast_date',
+            'date': 'FORECAST_DATE',
         })
     
     day_dict = df['day']
 
-    df_formatted['mintemp_f'] = pd.Series([key['mintemp_f'] for key in day_dict])    
-    df_formatted['maxtemp_f'] = pd.Series([key['maxtemp_f'] for key in day_dict])
-    df_formatted['avgtemp_f'] = pd.Series([key['avgtemp_f'] for key in day_dict])
-    df_formatted['maxwind_mph'] = pd.Series([key['maxwind_mph'] for key in day_dict])
-    df_formatted['totalprecip_in'] = pd.Series([key['totalprecip_in'] for key in day_dict])
-    df_formatted['avghumidity'] = pd.Series([key['avghumidity'] for key in day_dict])
+    df_formatted['MINTEMP_F'] = pd.Series([key['mintemp_f'] for key in day_dict])    
+    df_formatted['MAXTEMP_F'] = pd.Series([key['maxtemp_f'] for key in day_dict])
+    df_formatted['AVGTEMP_F'] = pd.Series([key['avgtemp_f'] for key in day_dict])
+    df_formatted['MAXWIND_MPH'] = pd.Series([key['maxwind_mph'] for key in day_dict])
+    df_formatted['TOTALPRECIP_IN'] = pd.Series([key['totalprecip_in'] for key in day_dict])
+    df_formatted['AVGHUMIDITY'] = pd.Series([key['avghumidity'] for key in day_dict])
 
     astro_dict = df['astro']
 
-    df_formatted['sunrise'] = pd.Series([key['sunrise'] for key in astro_dict])
-    df_formatted['sunset'] = pd.Series([key['sunset'] for key in astro_dict])
-    df_formatted['moonrise'] = pd.Series([key['moonrise'] for key in astro_dict])
-    df_formatted['moonset'] = pd.Series([key['moonset'] for key in astro_dict])
-    df_formatted['moon_phase'] = pd.Series([key['moon_phase'] for key in astro_dict])
-    df_formatted['moon_illumination'] = pd.Series([key['moon_illumination'] for key in astro_dict])
+    df_formatted['SUNRISE'] = pd.Series([key['sunrise'] for key in astro_dict])
+    df_formatted['SUNSET'] = pd.Series([key['sunset'] for key in astro_dict])
+    df_formatted['MOON_PHASE'] = pd.Series([key['moon_phase'] for key in astro_dict])
 
-    df_formatted['city'] =  pd.Series([location_series['name'] for x in range(len(df.index))])
-    df_formatted['state'] =  pd.Series([location_series['region'] for x in range(len(df.index))])
+    df_formatted['CITY'] =  pd.Series([location_series['name'] for x in range(len(df.index))])
+    df_formatted['STATE'] =  pd.Series([location_series['region'] for x in range(len(df.index))])
 
     return df_formatted
