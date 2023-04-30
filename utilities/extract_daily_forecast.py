43a102dc5ec9a5ea40883c6e88439b534efc9d5a
Rushil Shah <rps1073@gmail.com>
2023-04-29 21:47:16 -0400

    lint

--- utilities/extract_daily_forecast.py
+++ utilities/extract_daily_forecast.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def extract_daily_forecast(config):
     print("Querying API")
     url = f"https://api.weatherapi.com/v1/history.json?key={config['api_key']}&q={config['location']}&dt={config['start_date']}&end_dt={config['end_date']}"
--- utilities/load_daily_forecast.py
+++ utilities/load_daily_forecast.py
@@ -1,5 +1,6 @@
 from snowflake.connector.pandas_tools import write_pandas
 
+
 def load_daily_forecast(conn, forecast_df_parsed):
     print("Writing to db")
     write_pandas(conn, forecast_df_parsed, "DAILY_FORECAST")
